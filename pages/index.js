9557059d1a2cfcc77e19cfd3dc5e0bd9d1013eaf
Ondrej Sevcik <Ondrej Sevcik>
2021-05-02 11:32:15 +0200

    fixed size

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ export default function Home() {
     <Layout>
       <div className="h-xs-hero-no-nav md:h-md-hero-no-nav">
         <div className="absolute left-1/2 top-1/2 grid text-center gap-y-4" style={{ transform: 'translate(-50%, -50%)' }}>
-          <Title level={1} size={6} white style={{ marginBottom: '0rem' }}>
-            <span class="text-2xl">SOON...</span>
+          <Title level={1} white style={{ marginBottom: '0rem' }}>
+            <span class="text-3xl">SOON...</span>
           </Title>
         </div>
       </div>
