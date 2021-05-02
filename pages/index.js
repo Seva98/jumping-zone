5707beafa05894e77b657ca6a32f33dd8f21d95a
Ondrej Sevcik <Ondrej Sevcik>
2021-05-02 11:29:17 +0200

    title?

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="h-xs-hero-no-nav md:h-md-hero-no-nav">
         <div className="absolute left-1/2 top-1/2 grid text-center gap-y-4" style={{ transform: 'translate(-50%, -50%)' }}>
           <Title level={1} size={6} white style={{ marginBottom: '0rem' }}>
-            SOON...
+            <span class="text-2xl">SOON...</span>
           </Title>
         </div>
       </div>
